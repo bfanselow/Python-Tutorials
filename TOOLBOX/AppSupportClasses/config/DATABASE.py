f3ed8ff1ee4f5d8611d1073d1d584948b54fb73a
bfanselow <bfanselow@gmail.com>
2019-11-13 15:13:30 -0700

    Create DATABASE.py

---
+++ TOOLBOX/AppSupportClasses/config/DATABASE.py
@@ -0,0 +1,25 @@
+## database  config
+## storage_db_id: { <args> }
+
+workflow_test_applog =  {
+   'env':      'dev',
+   'host':     'localhost',
+   'db_name':  '******',
+   'db_user':  '******',
+   'db_pw':    '******'
+}
+applog_dev =  {
+   'env':      'dev',
+   'host':     '0.0.0.0',
+   'db_name':  '*******',
+   'db_user':  '********',
+   'db_pw':    '*******'
+}
+
+local_prod = {
+   'env':      'prod',
+   'host':     'my.server.com',
+   'db_name':  'workflow_prod',
+   'db_user':  '*******',
+   'db_pw':    '********'
+}
