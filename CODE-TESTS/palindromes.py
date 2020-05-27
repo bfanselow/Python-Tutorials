9f10d78e255b2225d31febe4961c6b0d2253dd26
bfanselow <bfanselow@gmail.com>
2020-05-27 11:23:09 -0600

    Create palindromes.py

---
+++ CODE-TESTS/palindromes.py
@@ -0,0 +1,27 @@
+"""
+
+  palindromes.py
+
+  Given a paragraph of text, identify if there are any "palidromes"
+  (words spelled the same forward or backword) not including single letter
+  words
+
+  Examples: civic, radar, level, rotor, kayak, reviver, racecar, madam, refer
+
+"""
+import re
+
+text = "Once upon a time, I paddeled a kayak to up the Northwest passage. I had no GPS but did occasionally did refer to a map. At one point during the trip a large motor boat almost hit me, which could have shredded me to bits with its rotor spinning like a forumla-1 racecar."
+
+def clean_text(in_string):
+  """
+   Remove commas, periods and other punctuation
+   Throw out any one-letter words
+  """
+  cleaned_str = re.sub('[,\.\!\?\:\;\(\)]', '', in_string)
+  cleaned_str = re.sub('\s\w\s', '', cleaned_str)
+
+  return cleaned_str
+
+plist = [word for word in  clean_text(text).split() if  word == word[::-1] ]
+print( plist )
