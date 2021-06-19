7def1cec5d9a0d97e67f2db36ae008786ecf4ee1
bfanselow <bfanselow@gmail.com>
2021-06-19 05:48:08 -0600

    Update call_by_str.py

--- call_by_str.py
+++ call_by_str.py
@@ -1,6 +1,26 @@
 """
- Demonstrate how to call a method using its (string) name, via getattr()
+
+  Demonstrating how to call methods useing their (string) name.
+  We use the getattr() function to get the value of a named attribute of an object.
+
+  How getattr() works:
+
+    ...
+    class Person:
+        age = 23
+        name = "Adam"
+
+    person = Person()
+    print('The age is:', getattr(person, "age"))
+    print('The age is:', person.age)
+    ...
+
+  It can also be used to get the value of a method.
+  method = getattr(object, method_name)
+  Calling the method is then done simply by appending parens to the returned value: method() 
+
 """
+method_names = ['bar1', 'bar2']
 
 class Foo:
     def bar1(self):
@@ -8,14 +28,21 @@ class Foo:
     def bar2(self):
         print('bar2')
 
+    def callall(self):
+        # Here, we use "self" for our first arg to getattr()
+        print("Calling from inside class...")
+        for mname in method_names: 
+            f = getattr(self, mname)
+            f()
+   
 
+if __name__ == '__main__':
 
-f = Foo()
-getattr(f, "bar1")()
+    f = Foo()
+    f.callall()
 
-# OR
-
-def call_method(o, name):
-    return getattr(o, name)()
-call_method(f, "bar2")
+    print("\n\nCalling from outside class...")
+    # Here, we have to instantiate the class for our first arg to getattr()
+    for mname in method_names: 
+        getattr(f, mname)()
 
