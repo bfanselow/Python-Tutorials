9bac20561ab43be461facb86c8098b2510e00efb
bfanselow <bfanselow@gmail.com>
2022-07-01 14:31:04 -0600

    Update abcClass.py

--- abcClass.py
+++ abcClass.py
@@ -6,7 +6,7 @@ class AbstractClassExample(ABC):
  
     def __init__(self, value):
         self.value = value
-        super().__init__()
+        print(f"Inside {__class__.__name__} Parent Class. Initialized start-value={self.value}")
    
     # all methods decorated with abstractmethod must be implemented by child classes 
     @abstractmethod
@@ -15,23 +15,32 @@ class AbstractClassExample(ABC):
 
     # we can have a non-abstract method implemented here which the child classes can call
     def do_non_abstract(self):
-        print(f"Inside {__class__.__name__} Parent Class. Initialized start-value={self.value}")
+        print(f" +PARENT {__class__.__name__} Doing something non-abstract!")
         
 
 
 class Add42(AbstractClassExample):
+    def __init__(self, value):
+        self.value = value
+        print(f"Inside {__class__.__name__} Child Class. Initialized start-value={self.value}")
+        super().__init__(value)
 
     def do_math(self):
         value = self.value + 42
-        print(f"Inside {__class__.__name__} Child Class. value={value}")
+        print(f" =CHILD {__class__.__name__} Doing math value={value}")
         return value
     
 class Mul42(AbstractClassExample):
+    def __init__(self, value):
+        self.value = value
+        print(f"Inside {__class__.__name__} Child Class. Initialized start-value={self.value}")
+        super().__init__(value)
    
     def do_math(self):
         value = self.value * 42
-        super().do_non_abstract()
-        print(f"Inside {__class__.__name__} Child Class. value={value}")
+        #super().do_non_abstract() # super() not need tho it is sometimes seen here in this context
+        self.do_non_abstract()
+        print(f" =CHILD {__class__.__name__} Doing math value={value}")
         return value
 
 my_num = 10
