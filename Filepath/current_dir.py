f4ac4d8bee514bfe28e9687c0c87ca295ea315b3
bfanselow <bfanselow@gmail.com>
2023-01-05 08:41:09 -0700

    Update current_dir.py

--- Filepath/current_dir.py
+++ Filepath/current_dir.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
-""" Get directory of *this* file """
-
+# Get directory of *this* file
 BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+data_path = Path(BASE_DIR).joinpath('data.csv'))
