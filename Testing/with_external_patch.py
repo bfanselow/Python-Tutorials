1474745484ef46007231b5fb184ca80ecd90574b
bfanselow <bfanselow@gmail.com>
2024-07-21 10:52:40 -0600

    Update with_external_patch.py

--- Testing/with_external_patch.py
+++ Testing/with_external_patch.py
@@ -1,7 +1,8 @@
 # Suppose we need to mock an external call the is used in an imported module
 
-from stdlib_mock_test import ...
+from stdlib_mock_test import my_isfile
 
 def test_with_external_patch():
-  ...
-  assert
+  object_path = "stdlib_mock_test.os.path.isfile"
+  with patch(object_path, return_value="Yes")
+      assert(my_isfile('foo/bar') == "Yes")
